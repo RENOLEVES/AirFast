3707c169de4590fc4341ff4b4c55fdb4e26692dc
renoleves <zzhao260985652@gmail.com>
2025-09-26 19:21:31 -0400

    Create Spring

---
+++ src/main/java/ca/mcgill/esce321/flightManagement/FlightManagementApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.esce321.flightManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementApplication.class, args);
+	}
+
+}
