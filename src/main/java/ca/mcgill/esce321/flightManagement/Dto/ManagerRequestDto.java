1a3e9267b3a0e950a36efa141dde35bcfb52c233
Vincent <hevincent23@gmail.com>
2025-10-26 17:39:26 -0400

    removed useless manager password

--- src/main/java/ca/mcgill/esce321/flightManagement/Dto/ManagerRequestDto.java
+++ src/main/java/ca/mcgill/esce321/flightManagement/Dto/ManagerRequestDto.java
@@ -23,7 +23,7 @@ public class ManagerRequestDto {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = password;
+        // this.password = password;
     }
 
     // Getters and setters
