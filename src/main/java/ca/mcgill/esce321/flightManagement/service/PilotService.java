adb9f8e86664a96905926f188d8c6415e43d070e
Vincent <hevincent23@gmail.com>
2025-11-05 16:36:34 -0500

    pilotService.java

--- src/main/java/ca/mcgill/esce321/flightManagement/service/PilotService.java
+++ src/main/java/ca/mcgill/esce321/flightManagement/service/PilotService.java
@@ -50,26 +50,105 @@ public class PilotService {
 
       public PilotResponseDTO createPilot(PilotRequestDTO dto) {
 
-        
-        // Date today = Date.valueOf(LocalDate.now());
-        
         Pilot pilotToCreate = new Pilot(dto.getEmail(), dto.getPassword(), dto.getFirstName(), dto.getLastName());
         Pilot saved = personRepository.save(pilotToCreate);
-        List<Long> newFlightIds = new ArrayList<Long>();
-
-        for (Flight e : saved.getFlights()) {
-            newFlightIds.add(e.getFlightId());
-            
-        }
+     
         return new PilotResponseDTO(
                 saved.getId(),
                 saved.getEmail(),
                 saved.getPassword(),
                 saved.getFirstName(),
                 saved.getLastName(),
-                newFlightIds
+                saved.getFlights().stream().map(Flight::getFlightId).toList()
                 );
     }
+
+    public PilotResponseDTO findPilotById(long id) {
+        Optional<Person> p = personRepository.findById(id);
+
+     
+        if (p.isPresent() && p.get() instanceof Pilot pilot) {
+
+            return new PilotResponseDTO(
+                    pilot.getId(),
+                    pilot.getEmail(),
+                    pilot.getPassword(),
+                    pilot.getFirstName(),
+                    pilot.getLastName(),
+                    pilot.getFlights().stream().map(Flight::getFlightId).toList()
+            );
+        } else {
+            throw new IllegalArgumentException("There is no Pilot with ID " + id + ".");
+        }
+    }
+
+     public List<PilotResponseDTO> findAllPilots() {
+        List<Person> allPersons = personRepository.findAll();
+        List<PilotResponseDTO> allPilots = new ArrayList<>();
+        for (Person p : allPersons) {
+            if (p instanceof Pilot pilot) {
+
+                allPilots.add(new PilotResponseDTO(
+                    pilot.getId(),
+                    pilot.getEmail(),
+                    pilot.getPassword(),
+                    pilot.getFirstName(),
+                    pilot.getLastName(),
+                    pilot.getFlights().stream().map(Flight::getFlightId).toList()));
+            }
+        }
+        if(allPilots.isEmpty()) {
+            throw new IllegalArgumentException("There are no Managers in the database.");
+        }
+        return allPilots;
+    }
+
+    public PilotResponseDTO updatePilot(long id, PilotRequestDTO dto) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+
+        if (optionalPerson.isPresent() && optionalPerson.get() instanceof Pilot pilotToUpdate) {
+            pilotToUpdate.setEmail(dto.getEmail());
+            pilotToUpdate.setFirstName(dto.getFirstName());
+            pilotToUpdate.setLastName(dto.getLastName());
+            pilotToUpdate.setPassword(dto.getPassword());
+
+
+            List<Flight> flights = flightRepository.findAllById(dto.getFlightIds());
+
+            pilotToUpdate.setFlights(flights);
+
+            personRepository.save(pilotToUpdate);
+
+            return new PilotResponseDTO(
+                    pilotToUpdate.getId(),
+                    pilotToUpdate.getEmail(),
+                    pilotToUpdate.getPassword(),
+                    pilotToUpdate.getFirstName(),
+                    pilotToUpdate.getLastName(),
+                    pilotToUpdate.getFlights().stream().map(Flight::getFlightId).toList());
+           
+        } else {
+            throw new IllegalArgumentException("No Pilot found with ID " + id);
+        }
+    }
+
+    public void deletePilot(long id) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isPresent() && optionalPerson.get() instanceof Pilot pilot) {
+            personRepository.delete(pilot);
+        } else {
+            throw new IllegalArgumentException("No Pilot found with ID " + id);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
