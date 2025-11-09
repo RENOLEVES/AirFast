122b027b9f434bcb0808ab98acbde2e4741fc170
Vincent <hevincent23@gmail.com>
2025-11-09 14:36:56 -0500

    IntegrationTests restructued to be CUD only

--- src/test/java/ca/mcgill/esce321/flightManagement/integrationTest/SeatIntegrationTests.java
+++ src/test/java/ca/mcgill/esce321/flightManagement/integrationTest/SeatIntegrationTests.java
@@ -1,4 +1,4 @@
-package ca.mcgill;
+package ca.mcgill.esce321.flightManagement.integrationTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
