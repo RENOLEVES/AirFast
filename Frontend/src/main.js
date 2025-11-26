726cb79698df7bfacf9e83bd9c8e2a02059ba1ac
renoleves <zzhao260985652@gmail.com>
2025-11-25 22:18:47 -0500

    update frontend

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VCalendar from 'v-calendar';
+import 'v-calendar/dist/style.css'; // Don't forget the required CSS!
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 2. Use the plugin to install it globally
+app.use(VCalendar, {});
+
+app.mount('#app');
 
